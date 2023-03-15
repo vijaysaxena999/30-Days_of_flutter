4de08c93e6bccfbff5beb8923a5a6af663a13aae
vijaysaxena999 <saxenavijay789@gmail.com>
2023-03-15 12:22:50 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,22 @@
+import 'package:flutter/material.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Material(
+        child: Center(
+          child: Container(
+            child: Text("Welcome To My First App"),
+          ),
+        ),
+      ),
+    );
+  }
+}
